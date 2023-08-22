29ac737a49ec598283904c2cdb1fb13f8532201e
Andrew Bell <fiixed@gmail.com>
2023-08-22 10:24:26 -0500

    yup validator

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,19 @@ import express from 'express';
 import 'dotenv/config';
 import './db';
 
+import authRouter from './routers/auth';
+
 const app = express();
 
-const PORT = process.env.PORT || 8987;
+// register our middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/auth', authRouter);
+
+
+
+const PORT = process.env.PORT || 8988;
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
