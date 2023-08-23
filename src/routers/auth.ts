9d4b135248f35c390ca1d9b4e787e5c31c9c792d
Andrew Bell <fiixed@gmail.com>
2023-08-23 09:17:56 -0500

    re-verify email

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
-import { create, verifyEmail } from '#/controllers/user';
+import { create, sendReVerificationToken, verifyEmail } from '#/controllers/user';
 import { validate } from '#/middleware/validator';
 import { CreateUserSchema, EmailVerificationBody } from '#/utils/validationSchema';
+import { sendVerificationMail } from '#/utils/mail';
 
 
 const router = Router();
 
 router.post('/create', validate(CreateUserSchema), create);
 router.post('/verify-email', validate(EmailVerificationBody), verifyEmail);
+router.post('/re-verify-email', sendReVerificationToken);
 
 export default router;
